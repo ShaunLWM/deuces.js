0284a7f62b6cc3dbed7273ca5fa0b03eb077f800
ShaunLWM <shaunlimweimin@gmail.com>
2020-02-18 22:47:53 +0800

    Fix undefined cards deck Fix unexported variable

--- test/go.js
+++ test/go.js
@@ -1,7 +1,6 @@
 const Card = require("../lib/Card");
 const Deck = require("../lib/Deck");
 const Evaluator = require("../lib/Evaluator");
-const LookupTable = require("../lib/LookupTable");
 let evaluator = new Evaluator()
 let card = Card.newCard("Qh");
 
@@ -10,6 +9,7 @@ let board = [
     Card.newCard('2s'),
     Card.newCard('Jc')
 ]
+
 let hand = [
     Card.newCard('Qs'),
     Card.newCard('Th')
@@ -19,3 +19,29 @@ Card.print_pretty_cards(board.concat(hand))
 
 rank = evaluator.evaluate(board, hand)
 console.log(`Rank for your hand is: ${rank}`);
+
+const deck = new Deck();
+board = deck.draw(5)
+player1_hand = deck.draw(2)
+player2_hand = deck.draw(2)
+
+console.log(`The board`);
+Card.print_pretty_cards(board)
+
+console.log("Player 1's cards:");
+Card.print_pretty_cards(player1_hand)
+
+console.log("Player 2's cards:");
+Card.print_pretty_cards(player2_hand)
+
+p1_score = evaluator.evaluate(board, player1_hand)
+p2_score = evaluator.evaluate(board, player2_hand)
+
+p1_class = evaluator.get_rank_class(p1_score)
+p2_class = evaluator.get_rank_class(p2_score)
+
+console.log(`Player 1 hand rank = ${p1_score} (${evaluator.class_to_string(p1_class)})`)
+console.log(`Player 2 hand rank = ${p2_score} (${evaluator.class_to_string(p2_class)})`)
+
+const hands = [player1_hand, player2_hand]
+evaluator.hand_summary(board, hands)
