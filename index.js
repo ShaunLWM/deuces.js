b7890b1bfb2fde2e4cb93f775ffadbe5a02a6525
Marco Lehmann <m99@posteo.de>
2022-08-05 11:29:39 +0200

    Fix export of `Deck`

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
     Card: require("./lib/Card"),
-    Deck: require("./lib/Card"),
+    Deck: require("./lib/Deck"),
     Evaluator: require("./lib/Evaluator"),
     LookupTable: require("./lib/LookupTable")
 };
